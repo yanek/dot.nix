5e1d52d0cbb83bf240fc46a57bce97e4f683e5a7
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-17 17:51:03 +0200

    2025-04-17 17:51:02+02:00
    64 current 2025-04-17 17:14:27 25.05.20250412.2631b0b 6.12.23 *
    2025-04-17 17:40 : id 14 -> /nix/store/fagcjp1yf4jc1klhf3144dnrx397miqx-home-manager-generation

--- build.sh
+++ build.sh
@@ -7,18 +7,17 @@ git add .
 git diff --staged --name-status
 
 echo "NixOS rebuilding..."
-sudo nixos-rebuild switch --flake . --impure &>nixos-switch.log || (
-  bat nixos-switch.log | rg error && false
-)
+tput setaf 0
+sudo nixos-rebuild switch --flake . --impure | sed -e 's/^/\t/;'
+tput sgr0
 
 echo "Home Manager rebuilding..."
-home-manager switch --flake . &>home-switch.log || (
-  bat home-switch.log | rg error && false
-)
+tput setaf 0
+home-manager switch --flake . | sed -e 's/^/\t/;'
+tput sgr0
 
 echo "Generating git commit..."
 gen="$(date --rfc-3339 s)\n\n$(nixos-rebuild list-generations | rg current)\n$(home-manager generations | head -1)"
-git commit -am "$(echo -e $gen)" &>git.log || (
-  bat git.log | rg error && false
-)
+git commit -am "$(echo -e $gen)"
+
 git log -n1 --oneline
