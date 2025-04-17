9ca40a0f653660bc4e2541daab729b00506fae7b
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-17 17:52:01 +0200

    2025-04-17 17:52:01+02:00
    64 current 2025-04-17 17:14:27 25.05.20250412.2631b0b 6.12.23 *
    2025-04-17 17:40 : id 14 -> /nix/store/fagcjp1yf4jc1klhf3144dnrx397miqx-home-manager-generation

--- build.sh
+++ build.sh
@@ -7,14 +7,10 @@ git add .
 git diff --staged --name-status
 
 echo "NixOS rebuilding..."
-tput setaf 0
 sudo nixos-rebuild switch --flake . --impure | sed -e 's/^/\t/;'
-tput sgr0
 
 echo "Home Manager rebuilding..."
-tput setaf 0
 home-manager switch --flake . | sed -e 's/^/\t/;'
-tput sgr0
 
 echo "Generating git commit..."
 gen="$(date --rfc-3339 s)\n\n$(nixos-rebuild list-generations | rg current)\n$(home-manager generations | head -1)"
