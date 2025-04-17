aec31181d8d1fb9580dd9572a65fdb5aeb8413e4
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 00:48:32 +0200

    2025-04-18 00:48:32+02:00
    68 current 2025-04-18 00:44:25 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 00:48 : id 19 -> /nix/store/6m02n67f226irw568dzhdcivy5hq7bnx-home-manager-generation

--- mod/usr/wezterm.nix
+++ mod/usr/wezterm.nix
@@ -4,28 +4,26 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-        local wezterm = require 'wezterm'
-      │ local config = wezterm.config_builder()
-      │
-      │ -- config.freetype_load_flags = "NO_HINTING"
-      │ -- config.freetype_load_target = "HorizontalLcd"
-      │ config.font_size = 11
-      │ config.font = wezterm.font {family = "Fira Code", weight = "Medium"}
-      │ config.max_fps = 120
-      │ config.default_cursor_style = "BlinkingBar"
-      │ config.cursor_thickness = 2
-      │ config.cursor_blink_rate = 800
-      │ config.cursor_blink_ease_in = "Constant"
-      │ config.cursor_blink_ease_out = "Constant"
-      │ config.window_decorations = "RESIZE"
-      │ config.hide_tab_bar_if_only_one_tab = true
-      │ config.window_padding = {
-      │   left = 8,
-      │   right = 8,
-          top = 8,
-          bottom =8,
+      return {
+        -- config.freetype_load_flags = "NO_HINTING"
+        -- config.freetype_load_target = "HorizontalLcd"
+        config.font_size = 11
+        config.font = wezterm.font {family = "Fira Code", weight = "Medium"}
+        config.max_fps = 120
+        config.default_cursor_style = "BlinkingBar"
+        config.cursor_thickness = 2
+        config.cursor_blink_rate = 800
+        config.cursor_blink_ease_in = "Constant"
+        config.cursor_blink_ease_out = "Constant"
+        config.window_decorations = "RESIZE"
+        config.hide_tab_bar_if_only_one_tab = true
+        config.window_padding = {
+           left = 8,
+           right = 8,
+           top = 8,
+           bottom =8,
         }
-        return config
+      }
     '';
   };
 }
