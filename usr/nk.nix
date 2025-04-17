5d12aebfb601ed7a340479671b69ded792a6bf32
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 01:38:40 +0200

    2025-04-18 01:38:39+02:00
    68 current 2025-04-18 00:44:25 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 01:38 : id 24 -> /nix/store/d1fw26cwy21q3bqvgyr5jsrxd7x8sf3c-home-manager-generation

--- usr/nk.nix
+++ usr/nk.nix
@@ -1,7 +1,8 @@
-{ pkgs, userSettings, ... }:
+{ userSettings, ... }:
 
 {
   imports = [
+    ../mod/usr/theme/${userSettings.theme}.nix
     ../mod/usr/git.nix
     ../mod/usr/multimedia.nix
     ../mod/usr/3d-printing.nix
@@ -15,53 +16,12 @@
     ../mod/usr/helix/helix.nix
   ];
 
-  stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${userSettings.theme}.yaml";
-
   home.username = "${userSettings.username}";
   home.homeDirectory = "${userSettings.homeDir}";
 
   home.sessionPath = [
     "${userSettings.homeDir}/.local/bin"
   ];
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.ibm-plex;
-      name = "IBM Plex Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.noto-fonts;
-      name = "Noto Sans";
-    };
-
-    monospace = {
-      package = pkgs.ibm-plex;
-      name = "IBM Plex Mono";
-    };
-  };
-
-  stylix.cursor = {
-    name = "Nordic-cursors";
-    package = pkgs.nordic;
-    size = 24;
-  };
-
-  gtk.enable = true;
-  # gtk.cursorTheme = {
-  #   name = "Nordic-cursors";
-  #   package = pkgs.nordic;
-  #   size = 24;
-  # };
-  # gtk.theme = {
-  #   name = "Nordic";
-  #   package = pkgs.nordic;
-  # };
-  gtk.iconTheme = {
-    name = "Nordic-blueish";
-    package = pkgs.nordic;
-  };
 
   programs.home-manager.enable = true;
   home.stateVersion = "24.11";
