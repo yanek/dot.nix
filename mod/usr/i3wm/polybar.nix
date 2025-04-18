b892dd1ee412f0216602ca09cf155cf4ce9a96f8
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 17:34:21 +0200

    2025-04-18 17:34:21+02:00
    74 current 2025-04-18 15:34:15 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 17:34 : id 53 -> /nix/store/vlaiax4irrsw9cq2p1z72z7p45yhrb9y-home-manager-generation

--- mod/usr/i3wm/polybar.nix
+++ mod/usr/i3wm/polybar.nix
@@ -6,7 +6,7 @@
     extraConfig = builtins.readFile ./polybar.ini;
     script = ''
       for m in $(polybar --list-monitors | ${pkgs.coreutils}/bin/cut -d":" -f1); do
-        MONITOR=$m polybar -rq top &
+        MONITOR=$m polybar --reload top &
       done
     '';
   };
