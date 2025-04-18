faea652dfdc2e8f8a750904a7ad669e2a5714357
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 15:58:18 +0200

    2025-04-18 15:58:18+02:00
    74 current 2025-04-18 15:34:15 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 15:58 : id 42 -> /nix/store/220ynwr5zjig3lxg1q8qn4rbi3zc7g77-home-manager-generation

--- mod/usr/i3wm/polybar.nix
+++ mod/usr/i3wm/polybar.nix
@@ -5,10 +5,7 @@
     enable = true;
     extraConfig = builtins.readFile ./polybar.ini;
     script = ''
-      killall -q polybar
-      while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-      polybar -rq top &
-      echo "Polybar started"
+      polybar -rq top
     '';
   };
 }
