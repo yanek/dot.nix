49b7ec77e12eb11d419c9b57132078a2ffda45ee
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 21:41:04 +0200

    2025-04-18 21:41:04+02:00
    74 current 2025-04-18 15:34:15 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 21:41 : id 56 -> /nix/store/i5pqcw3179cw90kchvgp5cnxbn15ksdx-home-manager-generation

--- mod/usr/librewolf.nix
+++ mod/usr/librewolf.nix
@@ -1,7 +1,9 @@
-{ inputs, ... }:
+{ inputs, userSettings, ... }:
 
 {
   imports = [ inputs.betterfox.homeManagerModules.betterfox ];
+
+  stylix.targets."librewolf".profileNames = [userSettings.username];
 
   programs.librewolf = {
     enable = true;
