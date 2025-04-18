dc716ad00cc82278d0d6d59665a3fd8bd30844b6
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-18 14:26:02 +0200

    2025-04-18 14:26:01+02:00
    73 current 2025-04-18 13:55:58 25.05.20250412.2631b0b 6.12.23 *
    2025-04-18 14:25 : id 36 -> /nix/store/jaqi4pmgvhg9s4pgsgpy687xszv9ksrf-home-manager-generation

--- mod/usr/i3wm/i3wm.nix
+++ mod/usr/i3wm/i3wm.nix
@@ -2,6 +2,8 @@
 
 let
   mod = "Mod4"; # Super
+  term = "wezterm";
+  menu = "rofi -show drun";
 in
 {
   imports = [
@@ -13,8 +15,8 @@ in
 
     config = {
       modifier = mod;
-      terminal = "wezterm";
-      menu = "rofi -show drun";
+      terminal = term;
+      menu = menu;
 
       fonts = {
         names = [ "Fira Code" ];
@@ -26,25 +28,86 @@ in
         outer = 0;
       };
 
-      keybindings = {
-        "${mod}+h" = "focus left";
-        "${mod}+j" = "focus down";
-        "${mod}+k" = "focus up";
-        "${mod}+l" = "focus right";
-
-        "${mod}+Shift+h" = "move left";
-        "${mod}+Shift+j" = "move down";
-        "${mod}+Shift+k" = "move up";
-        "${mod}+Shift+l" = "move right";
-
-      };
-
       window.border = 1;
       window.titlebar = false;
       floating.border = 1;
       floating.titlebar = false;
       floating.modifier = mod;
       bars = [ ];
+
+      keybindings = {
+        "${mod}+Return" = "exec ${term}";
+        "${mod}+Shift+q" = "kill";
+        "${mod}+d" = "exec ${menu}";
+
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+
+        "${mod}+Left" = "focus left";
+        "${mod}+Down" = "focus down";
+        "${mod}+Up" = "focus up";
+        "${mod}+Right" = "focus right";
+
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+
+        "${mod}+Shift+Left" = "move left";
+        "${mod}+Shift+Down" = "move down";
+        "${mod}+Shift+Up" = "move up";
+        "${mod}+Shift+Right" = "move right";
+
+        "${mod}+f" = "fullscreen toggle";
+        "${mod}+s" = "layout stacking";
+        "${mod}+w" = "layout tabbed";
+        "${mod}+e" = "layout toggle split";
+        "${mod}+Shift+space" = "floating toggle";
+        "${mod}+space" = "focus mode_toggle";
+
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+        "${mod}+6" = "workspace number 6";
+        "${mod}+7" = "workspace number 7";
+        "${mod}+8" = "workspace number 8";
+
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+        "${mod}+Shift+e" = "exec i3-nagbar -t warning -m 'Exit?' -b 'Yes' 'i3-msg exit'";
+
+        "${mod}+r" = "mode resize";
+      };
+
+      modes = {
+        resize = {
+          "h" = "resize shrink width 10 px or 10 ppt";
+          "j" = "resize grow height 10 px or 10 ppt";
+          "k" = "resize shrink height 10 px or 10 ppt";
+          "l" = "resize grow width 10 px or 10 ppt";
+
+          "Left" = "resize shrink width 10 px or 10 ppt";
+          "Down" = "resize grow height 10 px or 10 ppt";
+          "Up" = "resize shrink height 10 px or 10 ppt";
+          "Right" = "resize grow width 10 px or 10 ppt";
+
+          "Escape" = "mode default";
+          "Return" = "mode default";
+        };
+      };
 
       assigns = {
         "5" = [ { class = "^discord$"; } ];
