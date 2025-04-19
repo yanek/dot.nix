5546ef7883d741bcb4c8a99fd81ba697e7d4b20b
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-19 13:26:44 +0200

    2025-04-19 13:26:43+02:00
    79 current 2025-04-19 13:26:35 25.05.20250417.b024ced 6.12.23 *
    2025-04-19 12:29 : id 65 -> /nix/store/ryvgcfjdzj4aa6dhpzgfy6gy20ldnjiq-home-manager-generation

--- modules/system/xorg.nix
+++ modules/system/xorg.nix
@@ -8,7 +8,7 @@
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
 
   services.displayManager = {
-    # defaultSession = "none+i3";
+    defaultSession = "none+i3";
   };
 
   services.xserver = {
@@ -16,7 +16,7 @@
     xkb.layout = "eu";
 
     desktopManager.xterm.enable = false;
-    # windowManager.i3.enable = true;
+    windowManager.i3.enable = true;
   };
 
   # Disable mouse acceleration
