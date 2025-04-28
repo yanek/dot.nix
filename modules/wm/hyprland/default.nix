67058ea268ec3ecf33f568b13a40759184150e70
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-28 21:14:13 +0200

    2025-04-28 21:14:12+02:00
    94 current 2025-04-28 15:25:25 25.05.20250424.f771eb4 6.12.24 *
    2025-04-28 21:14 : id 143 -> /nix/store/lypqm948nrfvw2h294v5i8c18vdf1g7z-home-manager-generation

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -20,4 +20,5 @@
     NIXOS_OZONE_WL = "1";
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
   };
+
 }
--- modules/wm/hyprland/environment.nix
+++ modules/wm/hyprland/environment.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    ../../programs/waybar/waybar.nix
+  ];
+
   home.packages = with pkgs; [
     rofi-wayland
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "waybar"
+    ];
+  };
 }
