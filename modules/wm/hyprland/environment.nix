cb7b183a3275d3e63e973f16c2bf09c7393bf31f
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-28 21:30:18 +0200

    2025-04-28 21:30:18+02:00
    94 current 2025-04-28 15:25:25 25.05.20250424.f771eb4 6.12.24 *
    2025-04-28 21:30 : id 147 -> /nix/store/xaw8118ynx0yi5x2njj6wxzj16lxbywj-home-manager-generation

--- modules/wm/hyprland/environment.nix
+++ modules/wm/hyprland/environment.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, userSettings, ... }:
 
+let
+  wallpaper = "${userSettings.homeDir}/.nixos-config/modules/themes/${userSettings.theme}/wallpaper.png";
+in
 {
   imports = [
     ../../programs/waybar/waybar.nix
@@ -8,7 +11,16 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+  };
 
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = true;
+      preload = [ wallpaper ];
+      wallpaper = [ ",${wallpaper}" ];
+    };
   };
 
   wayland.windowManager.hyprland.settings = {
