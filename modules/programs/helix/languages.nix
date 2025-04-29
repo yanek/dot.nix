633b44d1c8319dab44565116af3da9b5b02ad258
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 11:41:47 +0200

    2025-04-29 11:41:47+02:00
    98 current 2025-04-29 01:55:54 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 11:41 : id 175 -> /nix/store/mbga4gmq8dvqxz09g17c51n52r7iqh4z-home-manager-generation

--- modules/programs/helix/languages.nix
+++ modules/programs/helix/languages.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.helix.languages = {
+    language-server = {
+      nixd = {
+        command = "${pkgs.nixd}/bin/nixd";
+      };
+    };
     language = [
       {
         name = "nix";
         auto-format = true;
         formatter.command = "${pkgs.alejandra}/bin/alejandra";
+        language-servers = ["nixd"];
       }
       {
         name = "odin";
