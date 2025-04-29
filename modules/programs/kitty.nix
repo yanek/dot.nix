1f7ff2b1024d250db5bcc89276a716df64203ce3
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 15:01:45 +0200

    2025-04-29 15:01:44+02:00
    99 current 2025-04-29 11:54:47 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 15:01 : id 199 -> /nix/store/dayvvxjqs2myyy01z34ad4accabi374v-home-manager-generation

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -1,11 +1,12 @@
-{ ... }:
-
-{
+{...}: {
   programs.kitty = {
     enable = true;
     shellIntegration = {
       enableFishIntegration = true;
       enableBashIntegration = true;
+    };
+    settings = {
+      window_padding_width = 10;
     };
   };
 }
