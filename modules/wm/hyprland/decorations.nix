426bc130e2ee12b0015f9d9db18cf1b9bcb594fd
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 14:28:56 +0200

    2025-04-29 14:28:55+02:00
    99 current 2025-04-29 11:54:47 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 14:28 : id 185 -> /nix/store/jr6s6vfqf0y96v5lq88hn7f7nfmyidb3-home-manager-generation

--- modules/wm/hyprland/decorations.nix
+++ modules/wm/hyprland/decorations.nix
@@ -4,5 +4,20 @@
     active_opacity = 1.0;
     inactive_opacity = 0.8;
     fullscreen_opacity = 1.0;
+
+    blur = {
+      enabled = true;
+      size = 6;
+      passes = 2;
+      new_optimizations = "on";
+      ignore_opacity = true;
+      xray = true;
+    };
+
+    shadow = {
+      enabled = true;
+      range = 30;
+      render_power = 3;
+    };
   };
 }
