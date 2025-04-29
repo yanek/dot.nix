ff1f5e4190d3f4eeb9f102bc84ad96622ee1d884
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 17:34:35 +0200

    2025-04-29 17:34:35+02:00
    101 current 2025-04-29 15:04:24 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 17:34 : id 246 -> /nix/store/9nwbskqj24a00wwv1glm675a1avzbw45-home-manager-generation

--- modules/programs/waybar/waybar.nix
+++ modules/programs/waybar/waybar.nix
@@ -65,6 +65,8 @@
       format-on = "󰂯";
       format-off = "BT-off";
       format-disabled = "󰂲";
+      format-connected-battery = "{device_battery_percentage}% 󰂯";
+      format-alt = "{device_alias} 󰂯";
       tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
       tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
       tooltip-format-enumerate-connected = "{device_alias}\n{device_address}";
