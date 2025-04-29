bad0efa578417112d6c684b7944d6f507976584b
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 14:51:50 +0200

    2025-04-29 14:51:49+02:00
    99 current 2025-04-29 11:54:47 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 14:51 : id 197 -> /nix/store/wykasxga7zxm77b1d3c68g6rdjibcz9v-home-manager-generation

--- modules/wm/hyprland/windowrules.nix
+++ modules/wm/hyprland/windowrules.nix
@@ -2,5 +2,13 @@
   wayland.windowManager.hyprland.settings.windowrule = [
     "float, title:^(Volume Control)$" # pavucontrol
     "float, title:^(Bluetooth Devices)$" # blueman
+
+    # Browser Picture in Picture
+    "float, title:^(Picture-in-Picture)$"
+    "pin, title:^(Picture-in-Picture)$"
+    "move 69.5% 4%, title:^(Picture-in-Picture)$"
+
+    # idleinhibit
+    "windowrule = idleinhibit fullscreen,class:([window])"
   ];
 }
