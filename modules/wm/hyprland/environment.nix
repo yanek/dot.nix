6de81e94a1163d92a768b988d5731d8f2dde54f1
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-30 00:11:27 +0200

    2025-04-30 00:11:27+02:00
    101 current 2025-04-29 15:04:24 25.05.20250424.f771eb4 6.12.24 *
    2025-04-30 00:11 : id 286 -> /nix/store/nklvzzv7986m29b9qwkxa0d1gp7nzcvj-home-manager-generation

--- modules/wm/hyprland/environment.nix
+++ modules/wm/hyprland/environment.nix
@@ -15,6 +15,20 @@ in {
     enable = true;
   };
 
+  programs.tofi = {
+    enable = true;
+    settings = {
+      width = "100%";
+      height = "100%";
+      border-width = 0;
+      outline-width = 0;
+      padding-left = "35%";
+      padding-top = "35%";
+      result-spacing = 25;
+      num-results = 5;
+    };
+  };
+
   programs.hyprlock = {
     enable = true;
     settings = {
