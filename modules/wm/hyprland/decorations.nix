c913e61b90a16d30499f4391b7bb8cf67aafefd4
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-29 14:32:00 +0200

    2025-04-29 14:32:00+02:00
    99 current 2025-04-29 11:54:47 25.05.20250424.f771eb4 6.12.24 *
    2025-04-29 14:31 : id 190 -> /nix/store/dizk4745kdh9c0rqbfa6m4hfjv0ifdbx-home-manager-generation

--- modules/wm/hyprland/decorations.nix
+++ modules/wm/hyprland/decorations.nix
@@ -2,7 +2,7 @@
   wayland.windowManager.hyprland.settings.decoration = {
     rounding = 10;
     active_opacity = 1.0;
-    inactive_opacity = 0.85;
+    inactive_opacity = 0.8;
     fullscreen_opacity = 1.0;
 
     blur = {
