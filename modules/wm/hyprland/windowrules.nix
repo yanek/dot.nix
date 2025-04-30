82711f29a7f9b47f61543daaf04e69b5b5552b5a
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-30 12:34:29 +0200

    2025-04-30 12:34:29+02:00
    101 current 2025-04-29 15:04:24 25.05.20250424.f771eb4 6.12.24 *
    2025-04-30 12:34 : id 323 -> /nix/store/12pbaj1kgz06720qcw9rddb2jx55f51m-home-manager-generation

--- modules/wm/hyprland/windowrules.nix
+++ modules/wm/hyprland/windowrules.nix
@@ -4,16 +4,17 @@
       "float, title:^(Volume Control)$" # pavucontrol
       "float, title:^(Bluetooth Devices)$" # blueman
 
-      "workspace name:comms, class:^(discord)$"
-      "workspace name:game, class:^(steam)$"
+      "workspace name:comms, class:discord"
+      "workspace name:game, class:steam"
 
       "noinitialfocus, class:discord"
       "noinitialfocus, class:steam"
 
       # Browser Picture in Picture
       "float, title:^(Picture-in-Picture)$"
+      "monitor DP-6, title:^(Picture-in-Picture)$"
       "pin, title:^(Picture-in-Picture)$"
-      "move 69.5% 4%, title:^(Picture-in-Picture)$"
+      "center, title:^(Picture-in-Picture)$"
     ];
     workspace = [
       "1, monitor:DP-6, defaultName:main"
