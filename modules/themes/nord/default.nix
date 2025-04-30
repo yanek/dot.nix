b49b4ca45f31738b2c6a7f6f545b830294e58404
Noé Ksiazek <noe.ksiazek@pm.me>
2025-04-30 23:49:01 +0200

    2025-04-30 23:49:01+02:00
    101 current 2025-04-29 15:04:24 25.05.20250424.f771eb4 6.12.24 *
    2025-04-30 21:18 : id 332 -> /nix/store/8ww1gr3sh9c0bjzx740zwiwkcsvghang-home-manager-generation

--- modules/themes/nord/default.nix
+++ modules/themes/nord/default.nix
@@ -3,44 +3,14 @@
   lib,
   config,
   ...
-}: let
-  nord = [
-    # nord0-3
-    # - Polar Night
-    "#2e3440"
-    "#3b4252"
-    "#434c5e"
-    "#4c566a"
-
-    # nord4-6
-    # - Snow Storm
-    "#d8dee9"
-    "#e5e9f0"
-    "#eceff4"
-
-    # nord7-10
-    # - Frost
-    "#8fbcbb"
-    "#88c0d0"
-    "#81a1c1"
-    "#5e81ac"
-
-    # nord11-15
-    # - Aurora
-    "#bf616a" # 11 - red
-    "#d08770" # 12 - orange
-    "#ebcb8b" # 13 - yellow
-    "#a3be8c" # 14 - green
-    "#b48ead" # 15 - purple
-  ];
-in {
+}: {
   imports = [
     ./fonts.nix
   ];
 
   stylix = {
     enable = true;
-    base16Scheme = ./nord.yaml;
+    base16Scheme = ./scheme.yaml;
     polarity = "light";
 
     cursor = {
@@ -73,7 +43,6 @@ in {
   };
 
   programs.tofi.settings = {
-    # selection-color = lib.mkForce (builtins.elemAt nord 14);
     selection-color = lib.mkForce (config.lib.stylix.colors.base0B);
   };
 }
