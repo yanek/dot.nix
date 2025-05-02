6cb3c3af7fdd3ed1aae0111b18b53b3ad9e2093d
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-02 02:07:37 +0200

    hyprland: autostart openrgb config

--- modules/wm/hyprland/startup.nix
+++ modules/wm/hyprland/startup.nix
@@ -6,6 +6,7 @@
       "[workspace 4 silent] sleep 5 && uwsm app -- kitty btop"
       "[workspace 4 silent] uwsm app -- discord"
       "[workspace 5 silent] uwsm app -- spotify"
+      "openrgb -m static -c cd3c0a"
       "[workspace 1 silent] uwsm app -- firefox"
       "[workspace 3 silent] uwsm app -- steam"
     ];
