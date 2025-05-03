2dd0109f2043d6b096c5754112124592e6c45766
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-04 01:05:09 +0200

    hyprland: update startup apps

--- modules/wm/hyprland/startup.nix
+++ modules/wm/hyprland/startup.nix
@@ -3,11 +3,12 @@
     exec-once = [
       "uwsm app -- hyprlock"
       "openrgb -m static -c cd3c0a"
-      "[workspace 5 silent] sleep 5 && uwsm app -- kitty cava"
-      "[workspace 5 silent] sleep 5 && uwsm app -- kitty btop"
+      "[workspace 5 silent] sleep 10 && uwsm app -- kitty cava"
+      "[workspace 5 silent] sleep 10 && uwsm app -- kitty nix shell nixpkgs#tty-clock -c tty-clock"
+      "[workspace 5 silent] sleep 8 && uwsm app -- kitty btop"
       "[workspace 5 silent] uwsm app -- webcord"
       "[workspace 6 silent] uwsm app -- spotify"
-      "[workspace 1 silent] uwsm app -- firefox"
+      "[workspace 1] uwsm app -- firefox"
       "[workspace 2 silent] uwsm app -- steam"
     ];
   };
