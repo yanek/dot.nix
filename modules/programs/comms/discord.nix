d63b28f6a0d99f8726ef4c960f50255889aea90f
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-10 01:39:20 +0200

    discord: vesktop -> nixcord

--- modules/programs/comms/discord.nix
+++ modules/programs/comms/discord.nix
@@ -1,5 +1,52 @@
 {...}: {
-  programs.vesktop = {
+  programs.nixcord = {
     enable = true;
+
+    discord = {
+      openASAR.enable = false;
+      vencord.enable = false;
+    };
+
+    vesktop = {
+      enable = true;
+    };
+
+    vesktopConfig = {
+      plugins = {
+        alwaysTrust = {
+          enable = true;
+          domain = true;
+          file = true;
+        };
+
+        betterFolders = {
+          enable = true;
+          closeAllFolders = true;
+          closeOthers = true;
+        };
+
+        clearURLs = {
+          enable = true;
+        };
+
+        emoteCloner = {
+          enable = true;
+        };
+
+        gameActivityToggle = {
+          enable = true;
+        };
+
+        typingIndicator = {
+          enable = true;
+          includeCurrentChannel = true;
+          indicatorMode = "both";
+        };
+
+        whoReacted = {
+          enable = true;
+        };
+      };
+    };
   };
 }
