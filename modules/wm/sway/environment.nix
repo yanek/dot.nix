683cbd31fd2262c1f1a21e1b258db52f90a3e55d
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-09 11:11:15 +0200

    tofi: clear cache on regen

--- modules/wm/sway/environment.nix
+++ modules/wm/sway/environment.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  config,
   lib,
   userSettings,
   ...
@@ -28,6 +29,14 @@
       fuzzy-match = true;
       font-size = lib.mkForce 14;
     };
+  };
+
+  home.activation = {
+    # https://github.com/philj56/tofi/issues/115#issuecomment-1701748297
+    regenerateTofiCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      tofi_cache=${config.xdg.cacheHome}/tofi-drun
+      [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
+    '';
   };
 
   # Audio visualizer
