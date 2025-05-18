1adc67e6a235e233af88ff25f0da382f811ab56e
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-18 14:20:10 +0200

    home: allow neovim to be set as default editor

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -6,22 +6,33 @@
 with lib; {
   options.myHome.neovim = {
     enable = mkEnableOption "neovim";
+    isDefaultEditor = mkOption {
+      default = false;
+      type = types.bool;
+    };
   };
 
-  config.programs.nvf = mkIf config.myHome.neovim.enable {
-    enable = true;
+  config = mkIf config.myHome.neovim.enable {
+    programs.nvf = {
+      enable = true;
 
-    settings = {
-      imports = [
-        ./options.nix
-        ./languages.nix
-        ./treesitter.nix
-      ];
-      vim = {
-        viAlias = true;
-        vimAlias = true;
-        lsp.enable = true;
+      settings = {
+        imports = [
+          ./options.nix
+          ./languages.nix
+          ./treesitter.nix
+        ];
+        vim = {
+          viAlias = true;
+          vimAlias = true;
+          lsp.enable = true;
+        };
       };
+    };
+
+    home.sessionVariables = mkIf config.myHome.neovim.isDefaultEditor {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
     };
   };
 }
