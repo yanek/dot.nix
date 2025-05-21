0d12ee626a4ba383fcd1fecc8ea21d4392084593
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-21 23:56:21 +0200

    home/picom: replace hm module with a custom one (animation support)

--- modules/home/window-manager/extras/picom.nix
+++ modules/home/window-manager/extras/picom.nix
@@ -1,25 +1,35 @@
 {
-  services.picom = {
-    enable = true;
-    fade = true;
-    fadeDelta = 4;
-    vSync = true;
-    shadow = false;
-    backend = "glx";
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  package = pkgs.picom;
+in {
+  home.packages = [
+    package
+  ];
 
-    opacityRules = [
-      "90:class_g = 'Rofi'"
-    ];
+  systemd.user.services.picom = {
+    Unit = {
+      Description = "Picom X11 compositor";
+      After = ["graphical-session.target"];
+      PartOf = ["graphical-session.target"];
+    };
 
-    settings = {
-      blur = {
-        method = "gaussian";
-        size = 4;
-        deviation = 5.0;
-      };
-      blur-background-exclude = [
-        "class_g = 'slop'"
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = lib.concatStringsSep " " [
+        "${lib.getExe package}"
+        "--config ${config.xdg.configFile."picom/picom.conf".source}"
       ];
+      Restart = "always";
+      RestartSec = 3;
     };
   };
+
+  xdg.configFile."picom/picom.conf".text = builtins.readFile ./picom.conf;
 }
