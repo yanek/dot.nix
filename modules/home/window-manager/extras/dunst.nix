80fa055e6e62e31c0b8f7950aba7caea01bfa8eb
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-22 20:55:29 +0200

    home/dunst: update visuals

--- modules/home/window-manager/extras/dunst.nix
+++ modules/home/window-manager/extras/dunst.nix
@@ -1,11 +1,25 @@
 {
+  config,
+  lib,
+  ...
+}:
+with lib; {
   services.dunst = {
     enable = true;
     settings = {
       global = {
         origin = "top-center";
-        frame-width = 2;
-        gap-size = 8;
+        offset = "(0,34)";
+        progress_bar = true;
+        frame_width = 1;
+        gap_size = 8;
+        corner_radius = 8;
+        format = "<b>%s</b>\n%b";
+        alignment = "left";
+      };
+      urgency_normal = {
+        background = mkForce config.lib.stylix.colors.withHashtag.base02;
+        frame_color = mkForce config.lib.stylix.colors.withHashtag.base04;
       };
     };
   };
