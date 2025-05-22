9045e986ce0ef656de5acff51fea53ffe0e51630
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-22 20:54:18 +0200

    home/themes: add rose-pine-dawn

---
+++ modules/home/themes/rose-pine-dawn/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  isTheme = config.myHome.theme.variant == "rose-pine-dawn";
+in {
+  config = lib.mkIf isTheme {
+    stylix = {
+      polarity = "light";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-dawn.yaml";
+      image = ./wallpaper.png;
+    };
+
+    programs.helix.settings.theme = lib.mkForce "rose_pine_dawn";
+  };
+}
