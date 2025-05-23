f6b56d328683e1287aa36d84d31f3231b4647eb0
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-23 18:18:03 +0200

    home/picom: fix config

--- modules/home/window-manager/extras/picom.nix
+++ modules/home/window-manager/extras/picom.nix
@@ -5,31 +5,26 @@
   ...
 }: let
   package = pkgs.picom;
+  inherit (lib) getExe concatStringsSep;
 in {
-  home.packages = [
-    package
-  ];
-
+  home.packages = [package];
   systemd.user.services.picom = {
     Unit = {
       Description = "Picom X11 compositor";
       After = ["graphical-session.target"];
       PartOf = ["graphical-session.target"];
     };
-
     Install = {
       WantedBy = ["graphical-session.target"];
     };
-
     Service = {
-      ExecStart = lib.concatStringsSep " " [
-        "${lib.getExe package}"
+      ExecStart = concatStringsSep " " [
+        "${getExe package}"
         "--config ${config.xdg.configFile."picom/picom.conf".source}"
       ];
       Restart = "always";
       RestartSec = 3;
     };
   };
-
   xdg.configFile."picom/picom.conf".text = builtins.readFile ./picom.conf;
 }
