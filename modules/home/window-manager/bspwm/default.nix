12e0064b2ebd18306613e854e52d8ff8fd81047c
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-24 02:00:55 +0200

    home/bspwm: enable xsession module

--- modules/home/window-manager/bspwm/default.nix
+++ modules/home/window-manager/bspwm/default.nix
@@ -27,42 +27,45 @@ in {
   };
 
   config = mkIf cfg.enable {
-    xsession.windowManager.bspwm = {
+    xsession = {
       enable = true;
-      package = pkgs.bspwm;
+      windowManager.bspwm = {
+        enable = true;
+        package = pkgs.bspwm;
 
-      settings = {
-        border_width = 2;
-        window_gap = cfg.windowGap;
-        split_ratio = 0.52;
-        borderless_monocle = false;
-        gapless_monocle = false;
-        focus_follows_pointer = true;
-        pointer_follows_monitor = true;
-        pointer_follows_focus = false;
-        pointer_modifier = "mod4";
-        pointer_action1 = "move";
-        pointer_action2 = "resize_size";
-        pointer_action3 = "resize_corner";
+        settings = {
+          border_width = 2;
+          window_gap = cfg.windowGap;
+          split_ratio = 0.52;
+          borderless_monocle = false;
+          gapless_monocle = false;
+          focus_follows_pointer = true;
+          pointer_follows_monitor = true;
+          pointer_follows_focus = false;
+          pointer_modifier = "mod4";
+          pointer_action1 = "move";
+          pointer_action2 = "resize_size";
+          pointer_action3 = "resize_corner";
+        };
+
+        # This bit of code generates 1 * `desktopPerMonitor` workspaces
+        # per configured monitor for this host/user.
+        monitors = let
+          desktopPerMonitor = 4;
+          monitorDesktopMap =
+            if cfg.monitors == []
+            then {}
+            else
+              listToAttrs (
+                imap1 (i: monitor: {
+                  name = monitor;
+                  value = genList (j: toString ((i - 1) * desktopPerMonitor + j + 1)) desktopPerMonitor;
+                })
+                cfg.monitors
+              );
+        in
+          monitorDesktopMap;
       };
-
-      # This bit of code generates 1 * `desktopPerMonitor` workspaces
-      # per configured monitor for this host/user.
-      monitors = let
-        desktopPerMonitor = 4;
-        monitorDesktopMap =
-          if cfg.monitors == []
-          then {}
-          else
-            listToAttrs (
-              imap1 (i: monitor: {
-                name = monitor;
-                value = genList (j: toString ((i - 1) * desktopPerMonitor + j + 1)) desktopPerMonitor;
-              })
-              cfg.monitors
-            );
-      in
-        monitorDesktopMap;
     };
   };
 
