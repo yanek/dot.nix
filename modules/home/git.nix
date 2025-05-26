0b10cc235631d5914d05c9b9cc825ae23a98cffa
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-26 10:53:59 +0200

    home: enable vimkeys in gitui

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -53,6 +53,16 @@ in
     };
     gitui = mkIf (cfg.gui == "gitui") {
       enable = true;
+      keyConfig =
+        # ron
+        ''
+          (
+            move_left: Some(( code: Char('h'), modifiers: "")),
+            move_right: Some(( code: Char('l'), modifiers: "")),
+            move_up: Some(( code: Char('k'), modifiers: "")),
+            move_down: Some(( code: Char('j'), modifiers: "")),
+          )
+        '';
     };
   };
 }
