f2a6003d28ce5cfc22e2f9cf156934fa708b895d
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-27 23:26:01 +0200

    flake: add neovim-nightly overlay

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
     nvf.url = "github:notashelf/nvf";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
 
+    neovim-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+    };
+
     spicetify = {
       url = "github:gerg-l/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,122 +30,122 @@
     };
   };
 
-  outputs =
-    {
-      nixpkgs,
-      nixpkgs-stable,
-      hm,
-      ...
-    }@inputs:
-    let
-      systemSettings = {
-        system = "x86_64-linux";
-        locale = "en_US.UTF-8";
-        bootMode = "uefi";
-        bootMountPath = "/boot";
-      };
+  outputs = {
+    nixpkgs,
+    nixpkgs-stable,
+    hm,
+    ...
+  } @ inputs: let
+    systemSettings = {
+      system = "x86_64-linux";
+      locale = "en_US.UTF-8";
+      bootMode = "uefi";
+      bootMountPath = "/boot";
+    };
 
-      userSettings = {
-        username = "nk";
-        fullname = "Noé Ksiazek";
-        email = "noe.ksiazek@pm.me";
-        dirs = {
-          home = "/home/${userSettings.username}";
-          config = "${userSettings.dirs.home}/.nixos-config";
-        };
+    userSettings = {
+      username = "nk";
+      fullname = "Noé Ksiazek";
+      email = "noe.ksiazek@pm.me";
+      dirs = {
+        home = "/home/${userSettings.username}";
+        config = "${userSettings.dirs.home}/.nixos-config";
       };
+    };
 
-      myOverlay = final: prev: {
-        jellytui = prev.callPackage ./pkgs/jellytui/package.nix { };
+    myOverlay = final: prev: {
+      jellytui = prev.callPackage ./pkgs/jellytui/package.nix {};
+    };
+
+    pkgs-stable = import nixpkgs-stable {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
       };
+    };
 
-      pkgs-stable = import nixpkgs-stable {
+    pkgs = import nixpkgs {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
+      };
+      overlays = [
+        myOverlay
+        inputs.neovim-overlay.overlays.default
+      ];
+    };
+  in {
+    nixosConfigurations = {
+      nkdtop = nixpkgs.lib.nixosSystem {
+        inherit pkgs;
         system = systemSettings.system;
-        config = {
-          allowUnfree = true;
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+          inherit systemSettings;
+          inherit userSettings;
         };
+        modules = [
+          ./hosts/nkdtop/configuration.nix
+        ];
       };
-
-      pkgs = import nixpkgs {
+      nkltop = nixpkgs.lib.nixosSystem {
         system = systemSettings.system;
-        config = {
-          allowUnfree = true;
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-stable;
+          inherit systemSettings;
+          inherit userSettings;
         };
-        overlays = [ myOverlay ];
-      };
-    in
-    {
-      nixosConfigurations = {
-        nkdtop = nixpkgs.lib.nixosSystem {
-          inherit pkgs;
-          system = systemSettings.system;
-          specialArgs = {
-            inherit inputs;
-            inherit pkgs-stable;
-            inherit systemSettings;
-            inherit userSettings;
-          };
-          modules = [
-            ./hosts/nkdtop/configuration.nix
-          ];
-        };
-        nkltop = nixpkgs.lib.nixosSystem {
-          system = systemSettings.system;
-          specialArgs = {
-            inherit inputs;
-            inherit pkgs-stable;
-            inherit systemSettings;
-            inherit userSettings;
-          };
-          modules = [
-            ./hosts/nkltop/configuration.nix
-          ];
-        };
-      };
-
-      homeConfigurations = {
-        "nk@nkdtop" = hm.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit pkgs;
-            inherit systemSettings;
-            inherit userSettings;
-          };
-          modules = [
-            ./users/nk.core.nix
-            ./users/nk.dtop.nix
-            inputs.stylix.homeModules.stylix
-            inputs.spicetify.homeManagerModules.spicetify
-            inputs.nvf.homeManagerModules.default
-            inputs.nixcord.homeModules.nixcord
-          ];
-        };
-
-        "nk@nkltop" = hm.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit pkgs;
-            inherit systemSettings;
-            inherit userSettings;
-          };
-          modules = [
-            ./users/nk.core.nix
-            ./users/nk.nkltop.nix
-            inputs.stylix.homeModules.stylix
-            inputs.spicetify.homeManagerModules.spicetify
-            inputs.nvf.homeManagerModules.default
-            inputs.nixcord.homeModules.nixcord
-          ];
-        };
-      };
-
-      devShells.${systemSettings.system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          go-task
-          vscode-langservers-extracted
+        modules = [
+          ./hosts/nkltop/configuration.nix
         ];
       };
     };
+
+    homeConfigurations = {
+      "nk@nkdtop" = hm.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit pkgs;
+          inherit systemSettings;
+          inherit userSettings;
+        };
+        modules = [
+          ./users/nk.core.nix
+          ./users/nk.dtop.nix
+          inputs.stylix.homeModules.stylix
+          inputs.spicetify.homeManagerModules.spicetify
+          inputs.nvf.homeManagerModules.default
+          inputs.nixcord.homeModules.nixcord
+        ];
+      };
+
+      "nk@nkltop" = hm.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit pkgs;
+          inherit systemSettings;
+          inherit userSettings;
+        };
+        modules = [
+          ./users/nk.core.nix
+          ./users/nk.nkltop.nix
+          inputs.stylix.homeModules.stylix
+          inputs.spicetify.homeManagerModules.spicetify
+          inputs.nvf.homeManagerModules.default
+          inputs.nixcord.homeModules.nixcord
+        ];
+      };
+    };
+
+    devShells.${systemSettings.system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go-task
+        vscode-langservers-extracted
+      ];
+    };
+  };
 }
