100240c61991df4f0b6ae3208483403b953e1112
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-29 01:39:00 +0200

    home: update starshipt prompt

--- modules/home/shell/starship.nix
+++ modules/home/shell/starship.nix
@@ -3,7 +3,8 @@
   config,
   ...
 }:
-with lib; {
+with lib;
+{
   programs.starship = with config.lib.stylix.colors.withHashtag; {
     enable = true;
     enableFishIntegration = true;
@@ -11,44 +12,58 @@ with lib; {
     enableTransience = true;
     settings = {
       format = concatStrings [
+        "[](fg:${base01})"
+        "[ ](bg:${base01})"
         "$username"
+        "[ ](bg:${base01})"
         "$hostname"
+        "[ ](bg:${base01})"
         "$directory"
+        "[ ](bg:${base01})"
         "$git_branch"
-        "$git_state"
+        "[ ](bg:${base01})"
+        "[](fg:${base01})"
         "$character"
       ];
       right_format = concatStrings [
+        "$nix_shell"
         "$cmd_duration"
       ];
       username = {
-        show_always = false;
-        format = "[ $user ]($style)";
+        show_always = true;
+        format = "[$user]($style)";
         style_user = "${base05} bg:${base01}";
         style_root = "${base08} bg:${base01}";
       };
       hostname = {
-        ssh_only = true;
+        ssh_only = false;
         style = "fg:${base0A} bg:${base01}";
-        format = "[ $ssh_symbol$hostname ]($style)";
+        format = "[$ssh_symbol$hostname]($style)";
       };
       directory = {
-        format = "[ $read_only$path ]($style)";
+        format = "[$read_only$path]($style)";
         read_only = "!";
         style = "fg:${base0E} bg:${base01}";
       };
       git_branch = {
-        format = "[ $symbol$branch(:$remote_branch) ]($style)";
+        format = "[$symbol$branch(:$remote_branch)]($style)";
         style = "fg:${base0D} bg:${base01}";
       };
       git_state = {
-        format = "[ \($state( $progress_current/$progress_total)\) ]($style)";
+        format = "[\($state($progress_current/$progress_total)\)]($style)";
         style = "fg:${base03} bg:${base01}";
       };
+      nix_shell = {
+        format = "[$state(\($name\))]($style)";
+        style = "fg:${base03}";
+        impure_msg = "";
+        pure_msg = "";
+        heuristic = true;
+      };
       character = {
-        success_symbol = "[ ](bg:${base02})";
-        error_symbol = "[ ](bg:${base08})";
-        vimcmd_symbol = "[ ](bg:${base0B})";
+        success_symbol = "";
+        error_symbol = "[×](fg:${base08})";
+        vimcmd_symbol = "[v](fg:${base0B})";
       };
       cmd_duration = {
         format = "[$duration]($style)";
