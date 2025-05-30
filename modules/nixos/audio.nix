85bc7f3c10270536eb9461d009c17a3a04045355
Noé Ksiazek <noe.ksiazek@pm.me>
2025-05-31 00:02:38 +0200

    nixos: enable rtkit with audio

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -3,12 +3,14 @@
   lib,
   ...
 }:
-with lib; {
+with lib;
+{
   options.mySystem.audio = {
     enable = mkEnableOption "audio";
   };
 
   config = mkIf config.mySystem.audio.enable {
+    security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
       alsa.enable = true;
