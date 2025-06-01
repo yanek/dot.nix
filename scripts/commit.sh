827cc3f3b7c3e79b60b5c3a916e5ae1546164005
Noé Ksiazek <noe.ksiazek@pm.me>
2025-06-02 01:38:19 +0200

    meta: remove scripts/

--- scripts/commit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-gen="$(date --rfc-3339 s)\n\n$(nixos-rebuild list-generations | rg current)\n$(home-manager generations | head -1)"
-git commit -am "$(echo -e $gen)"
-git log -n1 --oneline
