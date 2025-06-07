70f15069f16fa6ab2537dfcdd6854b05d0aca7bb
Noé Ksiazek <noe.ksiazek@pm.me>
2025-06-07 20:45:11 +0200

    home: fix i3 startup commands

--- modules/home/window-manager/i3/startup.nix
+++ modules/home/window-manager/i3/startup.nix
@@ -4,7 +4,7 @@
   lib,
 }:
 let
-  inherit (lib) getExe getExe';
+  inherit (lib) getExe';
   color = config.lib.stylix.colors.withHashtag;
 in
 [
@@ -13,17 +13,11 @@ in
     command = "xsetroot -cursor_name left_ptr";
   }
   {
-    command = "${getExe pkgs.slock}";
+    command = "slock";
   }
   {
     # Set the desktop background to a solid color, based on the applied color scheme.
     command = "${getExe' pkgs.hsetroot "hsetroot"} -solid \"${color.base02}\"";
-    always = true;
-  }
-  {
-    # Needed because workspace doesn't work otherwise.
-    # TODO: check if we can do something about the home manager module to start the service later.
-    command = "systemctl --user restart polybar";
     always = true;
   }
 ]
