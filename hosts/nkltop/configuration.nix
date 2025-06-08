73bd5987ece19e2eed6016abee4b3da959290688
Noé Ksiazek <noe.ksiazek@pm.me>
2025-06-08 21:35:36 +0200

    hosts: remove charge threshold (unsupported)

--- hosts/nkltop/configuration.nix
+++ hosts/nkltop/configuration.nix
@@ -49,10 +49,7 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        START_CHARGE_THRESH_BAT0 = 40;
-        STOP_CHARGE_THRESH_BAT0 = 100;
+        CPU_MAX_PERF_ON_BAT = 30;
       };
     };
   };
