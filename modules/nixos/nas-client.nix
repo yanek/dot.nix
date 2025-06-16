c30f351d9bd92afb501cce8e7070987dac69a20f
Noé Ksiazek <noe.ksiazek@pm.me>
2025-06-16 21:28:52 +0200

    nixos: reconfigure nas client to be rw

--- modules/nixos/nas-client.nix
+++ modules/nixos/nas-client.nix
@@ -5,7 +5,8 @@
   userSettings,
   ...
 }:
-with lib; {
+with lib;
+{
   options.mySystem.nas = {
     enable = mkEnableOption "nas";
     automount = mkOption {
@@ -15,14 +16,19 @@ with lib; {
   };
 
   config = mkIf config.mySystem.nas.enable {
-    environment.systemPackages = [pkgs.cifs-utils];
-    fileSystems."/mnt/helix" = {
+    environment.systemPackages = [ pkgs.cifs-utils ];
+    fileSystems."/mnt/nas" = {
       device = "//192.168.1.81/share";
       fsType = "cifs";
-      options = let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in
-        mkIf config.mySystem.nas.automount ["${automount_opts},credentials=/home/${userSettings.username}/.smb-secrets"];
+      options =
+        let
+          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+          inherit (config.users.users.${userSettings.username}) uid;
+          inherit (config.users.groups.users) gid;
+        in
+        mkIf config.mySystem.nas.automount [
+          "${automount_opts},credentials=/home/${userSettings.username}/.smb-secrets,uid=${toString uid},gid=${toString gid}"
+        ];
     };
   };
 }
